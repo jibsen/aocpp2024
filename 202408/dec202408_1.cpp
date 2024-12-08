e75ba99fd37d116cb61a78e9d13d99a57a183fa1
Joergen Ibsen <ji@ibse.dk>
2024-12-08 09:04:55 +0100

    Day 8 part one

---
+++ 202408/dec202408_1.cpp
@@ -0,0 +1,69 @@
+//
+// Advent of Code 2024, day 8, part one
+//
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+#include <print>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+auto get_antennas(const auto &map)
+{
+	std::unordered_map<char, std::vector<std::pair<int, int>>> antennas;
+
+	for (const auto &[y, line] : std::views::enumerate(map)) {
+		for (const auto &[x, ch] : std::views::enumerate(line)) {
+			if (ch != '.') {
+				antennas[ch].emplace_back(x, y);
+			}
+		}
+	}
+
+	return antennas;
+}
+
+int main()
+{
+	std::vector<std::string> map(std::istream_iterator<std::string>{std::cin},
+	                             std::istream_iterator<std::string>{});
+
+	auto antennas = get_antennas(map);
+
+	for (const auto &[_, positions] : antennas) {
+		for (const auto &[lhs, rhs] : std::views::cartesian_product(positions, positions)) {
+			if (lhs == rhs) {
+				continue;
+			}
+
+			auto within_map = [&map](int x, int y) {
+				return x >= 0 && x < std::ssize(map.front())
+				    && y >= 0 && y < std::ssize(map);
+			};
+
+			auto [x1, y1] = lhs;
+			auto [x2, y2] = rhs;
+
+			const std::array<std::pair<int, int>, 2> antinode_positions = {{
+				{ x1 + (x1 - x2), y1 + (y1 - y2) },
+				{ x2 + (x2 - x1), y2 + (y2 - y1) }
+			}};
+
+			for (auto [x, y] : antinode_positions) {
+				if (within_map(x, y)) {
+					map[y][x] = '#';
+				}
+			}
+		}
+	}
+
+	// Count number of #s
+	auto num_antinodes = std::ranges::count(map | std::views::join, '#');
+
+	std::println("{} antinodes", num_antinodes);
+}
